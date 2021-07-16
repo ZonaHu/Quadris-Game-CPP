4c394e4722173ba1980e6ee19e713b48a62288ea
ZonaHu <hzm5622@126.com>
2021-07-17 00:42:23 +0800

    Added main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+// Derived from GenericBlock
+// Created by Simran Thind, Janakitti Ratana-Rueangsri, Zuomiao Hu
+// on 2021-07-16.
+
+#include <iostream>
+
+int main(){
+  // Creates an instance of GameManager, passing in the CLI args.
+
+
+}
