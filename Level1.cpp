81c19b79abaa970ee7f67264c7fc4aa20553192b
ZonaHu <hzm5622@126.com>
2021-07-25 04:20:16 +0800

    Completed level1

--- Level1.cpp
+++ Level1.cpp
@@ -12,9 +12,35 @@ Level1::Level1(const std::shared_ptr<BoardModel>& p) {
 }
 
 GenericBlock Level1::generateNextBlock() {
-
-  GenericLevel::generateNextBlock();
-
+  // generate the next block in level 1
+  GenericBlock res;
+  BlockType curType;
+  int num = rand() % 12;
+  // S and Z are selected with a probability of 1/12 each
+  if (num == 0){
+    curType = S_BLOCK;
+  }
+  else if (num == 1){
+    curType = Z_BLOCK;
+  }
+  // other blocks are selected with a probability of 1/6 each (and no star block in this level)
+  else if (num == 2 || num == 3){
+    curType = T_BLOCK;
+  }
+  else if (num == 4 || num == 5){
+    curType = O_BLOCK;
+  }
+  else if (num == 6 || num == 7){
+    curType = L_BLOCK;
+  }
+  else if (num == 8 || num == 9){
+    curType = J_BLOCK;
+  }
+  else if (num == 10 || num == 11){
+    curType = I_BLOCK;
+  }
+  res.setType(curType); // set the current type in the generic block
+  return res;
 }
 
 void Level1::postMoveOperation() {
@@ -22,5 +48,5 @@ void Level1::postMoveOperation() {
 }
 
 void Level1::postDropOperation() {
-  GenericLevel::postDropOperation();
+  // do nothing in level 1
 }
