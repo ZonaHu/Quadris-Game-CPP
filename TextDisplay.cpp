9dd6bfb5d4f3476f76abdad63d2e27680fa79920
ZonaHu <hzm5622@126.com>
2021-07-28 02:44:11 +0800

    implementing the update function

--- TextDisplay.cpp
+++ TextDisplay.cpp
@@ -16,7 +16,27 @@ TextDisplay::~TextDisplay() {
 }
 
 void TextDisplay::update() {
+  // this function prints out all
+  std::cout << "Level:        " << boardModel_.getLevel() << std::endl;
+  std::cout << "Score:        " << boardModel_.getScore() << std::endl;
+  std::cout << "Hi Score:     " << boardModel_.getHiScore() << std::endl;
+  std::cout << "-----------------" << std::endl;
+  int count = 1; // count denotes the current line number
+  while (count < 19){
+    if (count>=1&&count<=9){
+      std::cout << " " << count << "   "; // 1 digit numbers will have an extra space at the beginning fot format
+    }
+    else{
+      std::cout << count << "   "; // 2 digit numbers
+    }
+    // print the rest of things in the same line corresponding to count
 
+
+
+
+
+    count++; // increment the current line number
+  }
 
 }
 
